cdefd1a6ea5bd8388bdd261a1367d0ecda6264dc
kusumsgit <kusumsharma424@gmail.com>
2023-02-26 15:23:03 -0600

    Added a new script based on lab253 challenge

---
+++ lab253.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+C=1
+Str="Kusum"
+Filename="${Str}${C}"
+while [ -f $Filename ]
+do
+        if [ $C -eq 1 ]
+        then
+        echo "Existing files"
+        fi
+	if [ -f $Filename ]
+	then
+		echo "Existing Files"
+	fi
+        echo $Filename
+        ((C=C+1))
+        Filename="${Str}${C}"
+done
+if [ $C -eq 1 ]
+then
+        echo "Existing Files = 0"
+        echo "                   "
+fi
+((I=C+25))
+echo "New Files"
+while [ $C -lt $I ]
+do
+	Filename="${Str}${C}"
+	if [ ! -f $Filename ]
+	then
+		echo $Filename
+		touch $Filename
+	fi
+	((C=C+1))
+done
